993ad28ceda9c1dbf49a95848220938fff2d9c1d
HumbleGuy047 <92243872+HumbleGuy047@users.noreply.github.com>
2023-07-26 13:42:55 -0700

    Update 6_摄像头读取.py

--- 6_摄像头读取.py
+++ 6_摄像头读取.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)		#0表摄像头
-cap = cv2.VideoCapture('dataset/公路来往车辆2.mp4')		#读取视频
-cv2.namedWindow("video",0)		#使得窗口名为video的窗口可以改变大小
+# cap = cv2.VideoCapture(0)							   # 0 means computer camera
+cap = cv2.VideoCapture('dataset/公路来往车辆2.mp4')		# read video
+cv2.namedWindow("video",0)		# enable resizing of window
 while True:
 	ret,frame = cap.read()
 	if ret:
-		cv2.imshow('video',frame)		#显示前景
+		cv2.imshow('video',frame)		# show image
 		key = cv2.waitKey(20)
 		if key == 27:	#esc
 			break
 
-#释放资源
+#release resource
 cap.release()
 cv2.destroyAllWindows()
