ee71ae7924514bfc9128d236658c828544901497
HumbleGuy047 <92243872+HumbleGuy047@users.noreply.github.com>
2023-07-26 13:32:20 -0700

    done

--- 6_摄像头读取.py
+++ 6_摄像头读取.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)		#0表摄像头
-#cap = cv2.VideoCapture('../dataset/公路来往车辆2.mp4')		#读取视频
+cap = cv2.VideoCapture('dataset/公路来往车辆2.mp4')		#读取视频
 cv2.namedWindow("video",0)		#使得窗口名为video的窗口可以改变大小
 while True:
 	ret,frame = cap.read()
